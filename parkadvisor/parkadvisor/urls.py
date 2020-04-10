8e84a3a5b589fb32185279c389f226185db59707
seemabmahajan <seemabmahajan@gmail.com>
2020-04-10 15:45:51 -0700

    Login Page

--- parkadvisor/parkadvisor/urls.py
+++ parkadvisor/parkadvisor/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
+    path('accounts/password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>',  views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done',  views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
