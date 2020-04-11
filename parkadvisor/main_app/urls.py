b7f2faefe7a2c55ac4265d494337c123316569ab
lsack <lauragsack@gmail.com>
2020-04-10 19:56:24 -0700

    commits url and views updates

--- parkadvisor/main_app/urls.py
+++ parkadvisor/main_app/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('parks/', views.parks_index, name ='index'),
     path('about/', views.about, name='about'),
+    path('parks/<int:park_id>/', views.parks_detail, name='detail'),
+    # path('parks/<int:park_id>/add_review', views.add_review, name='add_review'),
+    # path('parks/<int:park_id>/assoc_review/<int:review_id>/', views.assoc_review, name='assoc_review'),
+    # path('reviews/', views.ReviewList.as_view(), name='reviews_index'),
+    # path('reviews/<int:pk>/', views.ReviewDetail.as_view(), name='reviews_detail'),
+    # path('reviews/<int:pk>/update/', views.ReviewUpdate.as_view(), name='reviews_update'),
+    # path('reviews/<int:pk>/delete/', views.ReviewDelete.as_view(), name='reviews_delete')
 ]
--- parkadvisor/main_app/views.py
+++ parkadvisor/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Park, Review,User
+from .models import Park, Review, User
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 
 class Cat:  # Note that parens are optional if not inheriting from another class
@@ -16,13 +18,21 @@ cats = [
 ]
 
 # Create your views here.
-from django.http import HttpResponse
-
 def home(request):
-    return redirect(request, '/parks')
+    return redirect(request, 'parks/')
 
 def about(request):
     return render(request, 'about.html')
 
 def parks_index(request):
     return render(request, 'parks/index.html', { 'cats': cats })
+
+def parks_detail(request, park_id):
+    park = Park.objects.get(id=park_id)
+    # review_form = ReviewForm()
+    this_parks_reviews = park.review_set.all()
+    return render (request, 'parks/detail.html', {
+        'park': park,
+        # 'review_form': review_form,
+        'reviews': this_parks_reviews
+    })
