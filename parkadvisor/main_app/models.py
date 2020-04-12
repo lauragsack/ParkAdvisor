43cb47a08960468f4259363286fec415f352b583
lsack <lauragsack@gmail.com>
2020-04-11 18:05:46 -0700

    commits review view fixes

--- parkadvisor/main_app/models.py
+++ parkadvisor/main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-CATEGORIES = (('L', 'Loding'), ('A', 'Activities'), ('F', 'Food'))
+CATEGORIES = (('L', 'Lodging'), ('A', 'Activities'), ('F', 'Food'))
 
 class Park(models.Model):
 	name = models.CharField(max_length=100)
--- parkadvisor/main_app/views.py
+++ parkadvisor/main_app/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Park, Review, User
 # todo: delete below line if go with CBVs
 from .forms import ReviewForm
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -29,22 +27,21 @@ def parks_detail(request, park_id):
         'reviews': this_parks_reviews
     })
 
-# todo: do we need this?
-# def assoc_review(request, park_id, review_id):
-#     Park.objects.get(id=park_id).reviews.add(review_id)
-#     return redirect('detail', park_id=country_id)
+def add_review(request, park_id):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            new_review = form.save(commit=False)
+            new_review.park_id = park_id
+            new_review.user = request.user
+            new_review.save()   
+            return redirect('detail', park_id)
+    else:
+        form = ReviewForm()
+    context = { 'form': form }
+    return render(request, 'reviews/review_form.html', { 'form': form })
 
 @login_required
-def add_review(request, park_id):
-    form = ReviewForm(request.POST)
-    if form.is_valid():
-        new_review = form.save(commit=False)
-        new_review.park_id = park_id
-        review.user = request.user
-        new_review.save()
-    return redirect('detail', pard_id=park_id)
-
-# @login_required
 def reviews_update(request, review_id):
     review = Review.objects.get(id=review_id)
     park_id = review.park.id
@@ -57,65 +54,32 @@ def reviews_update(request, review_id):
         form = ReviewForm(instance=review)
     return render(request, 'reviews/review_form.html', { 'form': form })
 
-# @login_required
+@login_required
 def reviews_delete(request, review_id):
-    print("review_id", review_id)
+    review = Review.objects.get(id=review_id)
     park_id = review.park.id
-    print("park_id", park_id)
     Review.objects.get(id=review_id).delete()
     return redirect('detail', park_id)
 
-# todo: add to logged in navbar
+
 @login_required
 def reviews_index(request):
     reviews = Review.objects.filter(user=request.user)
     return render(request, 'reviews/review_list.html', { 'reviews': reviews })
 
-# def reviews_detail(request, review_id):
-#     review = Review.objects.get(id=review_id)
-#     review_form = ReviewForm()
-#     return render (request, 'parks/detail.html', {
-#         'park': park,
-#         'review_form': review_form
-#     })
-
-
-# todo add this to base html nav: <li><a href="{% url 'signup' %}">Sign Up</a></li>
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
 
 
-# class ReviewList(ListView):
-#     model = Review
-
-# class ReviewDetail(DetailView):
-#     model = Review
-
-# class ReviewCreate(LoginRequiredMixin, CreateView):
-#     model = Review
-#     fields = '__all__'
-
-# class ReviewUpdate(LoginRequiredMixin, UpdateView):
-#     model = Review
-#     fields = '__all__'
-
-# class ReviewDelete(LoginRequiredMixin, DeleteView):
-#     model = Review
-#     success_url = '/parks/index.html/'
